1a96f1db7965cae764862c91de85d78646ea7b2b
the thing <65288778+mohamedfffff@users.noreply.github.com>
2026-01-11 17:47:44 +0200

    adding auth-success end point, index and dashboard pages, jwtutils(to-do)

--- src/main/java/com/example/spring_oauth2/HomeController.java
+++ src/main/java/com/example/spring_oauth2/HomeController.java
@@ -1,25 +1,40 @@
 package com.example.spring_oauth2;
 
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-@RestController
+
+@Controller
 public class HomeController {
+
+    private JwtUtils jwtUtils;
+
+    public HomeController(JwtUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+    }
     
     @GetMapping("/")
-    public String Home() {
-        return "Welcome! <a href='/user'>Click here to login</a>";
+    public String index() {
+        return "index";
     }
 
-    @GetMapping("/user")
-    public Map<String, Object> getUser(@AuthenticationPrincipal OAuth2User principal) {
-        return principal.getAttributes();
+    @GetMapping("/auth-success")
+    public String authSuccess(@AuthenticationPrincipal OAuth2User principal, Model model) {
+        String email = principal.getAttribute("email");
+        String name = principal.getAttribute("name");
+
+        String token =  jwtUtils.generateToken(email, name);
+
+        model.addAttribute("email", email);
+        model.addAttribute("name", name);
+        return "dashboard";
     }
     
 }
--- src/main/java/com/example/spring_oauth2/SecurityConfig.java
+++ src/main/java/com/example/spring_oauth2/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.spring_oauth2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,7 +17,9 @@ public class SecurityConfig {
                 .requestMatchers("/").permitAll()
                 .anyRequest().authenticated()
             )
-            .oauth2Login(Customizer.withDefaults());
+            .oauth2Login(oauth -> oauth
+                .defaultSuccessUrl("/auth-success")
+            );
 
         return http.build();
     }
